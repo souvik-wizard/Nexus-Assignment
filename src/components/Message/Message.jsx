6c823e085fdfae37f2fceb8e85f2e79d0b3329f8
Souvik Das <103851921+souvik-wizard@users.noreply.github.com>
2023-09-02 23:15:57 +0530

    Update Message.jsx

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -4,7 +4,7 @@ const Message = () => {
   return (
     <section className='text-black py-[50px]'>
       <div className='lg:py-[150px] lg:px-[40px] py-[80px] px-[20px] flex items-center flex-col'>
-        <h1 className='font-jost text-2xl lg:text-4xl text-center mb-[20px]'>‘Life never becomes a habit to me. It's always a marvel.’</h1>
+        <h1 className='font-jost text-2xl lg:text-4xl text-center mb-[20px]'>‘Life never becomes a habit to me. It&apos;s always a marvel.’</h1>
         <p>Katherine Mansfield</p>
       </div>
     </section>
